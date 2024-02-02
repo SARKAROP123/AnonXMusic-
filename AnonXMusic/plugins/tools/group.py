dda2687824adee2bdd80e982e452b094052ebc22
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-02-02 10:50:56 +0530

    Update group.py
    Signed-off-by: SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>

--- AnonXMusic/plugins/tools/group.py
+++ AnonXMusic/plugins/tools/group.py
@@ -21,7 +21,7 @@ async def brah3(app: app, message: Message):
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
-            text += f"[{user.first_name}](tg://user?id={user.id}) "
+            text = f"➻ {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :**\n\n**➻ **"
             x += 1
         except Exception:
             pass
